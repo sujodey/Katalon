3c2ddc8320fcb16063d8655ca06e50b3c8a30a9f
Administrator <nowsudheer@gmail.com>
2020-08-11 15:53:35 +0530

    changes made

---
+++ Sample API Test/Test Suites/UserTestSuite/UserTS.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UserTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>94715ddc-8a76-4179-aca7-6941658060ac</testSuiteGuid>
+   <testCaseLink>
+      <guid>5cea31be-484f-4356-8a5f-24f3e2ac2548</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserTestCase/ValidateUserDetails</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
